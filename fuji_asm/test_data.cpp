06a195ab930e94264cd6a8f2ca059344c2b62a20
BeyondCloud <a1989796@yahoo.com.tw>
2017-06-11 21:44:27 +0800

    convert string to hex

--- fuji_asm/test_data.cpp
+++ fuji_asm/test_data.cpp
@@ -6,7 +6,15 @@
     
 .STACK  200H	;this will change alot
 .DATA	;no
-    ;uRval  DW   1
-           ;DB   CR, LF, '$'  
+   Start1  DB   "%% Hello, World! Execution starts....", BEL, CR, LF, '$'
+   size1   DW   $-start1  ;29 00 ,29h = 41 = sizeof start1 string 
+   Goodbye DB   "%% Program stops. Exit now!",           BEL, CR, LF, '$'
+   size2   DW   $-Goodbye
+
+    uMSG   DB   CR, LF, "   Sum(1 .. "
+    uIdgt  DB  1
+           DB   ") = "
+    uRval  DW   1
+           DB   CR, LF, '$'
 .CODE	;no
 END
